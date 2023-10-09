550ec14e4fdb0d3096629a9e1b3ef9c0e1fff257
rpm3 <mancuso.b3@gmail.com>
2023-10-09 16:39:44 -0500

    Added the rest of the daily work hours to HTML, finished current DTG display.

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,11 @@
-var timeDisplay = $('#currentDay');
+var currentTime = dayjs();
+var dateDisplayEl = $('#currentDay');
 
 
 
 function displayDate() {
-  var todaysDate = dayjs().format('dddd, D MMMM YYYY');
-  timeDisplay.text(todaysDate);
+  var todaysDate = dayjs().format('[Today is ]dddd MMMM D, YYYY [and it is currently] h:m:s');
+  dateDisplayEl.text(todaysDate);
 }
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
@@ -23,12 +24,13 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
+
 });
 
 displayDate();
